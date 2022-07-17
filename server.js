a6a5a70e2e08ea4df6ea1f97dae65c22555ab503
owaisanwar <notowais09@gmail.com>
2022-07-17 11:02:50 +0500

    added package.json file

--- server.js
+++ server.js
@@ -213,4 +213,4 @@ const server = http.createServer((req, res) => {
 
 const port = 5000;
 
-server.listen(port, () => console.log('Server is listening on port', port));
+server.listen(process.env.PORT || port, () => console.log('Server is listening on port', port));
